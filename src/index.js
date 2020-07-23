e61699e151336c728ca0ab7d3727792f66154c0a
Kevin Truong <kevint@Kevin-MacBook-Pro.local>
2020-07-23 09:25:14 -0700

    Sucessful Register

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { SipProvider } from 'react-sip';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <SipProvider
+        host="http://209.182.219.239"
+        port={8631}
+        pathname="/ws" // Path in socket URI (e.g. wss://sip.example.com:7443/ws); "" by default
+        user="1001"
+        password="pppassword" // usually required (e.g. from ENV or props)
+        autoRegister={true} // true by default, see jssip.UA option register
+        autoAnswer={false} // automatically answer incoming calls; false by default
+        iceRestart={false} // force ICE session to restart on every WebRTC call; false by default
+        sessionTimersExpires={120} // value for Session-Expires header; 120 by default
+        // extraHeaders={{ // optional sip headers to send
+        //     register: ['X-Foo: foo', 'X-Bar: bar'],
+        //     invite: ['X-Foo: foo2', 'X-Bar: bar2']
+        // }}
+        // iceServers={[ // optional
+        //     { urls: ['stun:a.example.com', 'stun:b.example.com'] },
+        //     { urls: 'turn:example.com', username: 'foo', credential: '1234' }
+        // ]}
+        debug={true} // whether to output events to console; false by default
+    >
+        <App />
+    </SipProvider>,
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
